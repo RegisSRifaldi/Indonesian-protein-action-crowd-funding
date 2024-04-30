79b5d5a665a053576bbf619c153a050226ad1588
Regis <regisrifaldi8@gmail.com>
2024-04-30 18:24:03 +0700

    initial commit

---
+++ client/vite.config.js
@@ -0,0 +1,20 @@
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), nodePolyfills()],
+  define: {
+    "process.env": {},
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      target: "es2020",
+    },
+  },
+
+  build: {
+    target: "es2020",
+  },
+});
